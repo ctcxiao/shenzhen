82772e922af1b96dcab678fb4638a2b4d70afc7f
tcchen <tcchen@thoughtworks.com>
2018-09-03 17:59:53 +0800

    [Cheng] implement compare two collections is the same or not.

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+    collection_b = collection_b.sort();
+    let isSame = true;
+    collection_a.sort().forEach(function (value, index) {
+        if (value !== collection_b[index]){
+          isSame = false;
+        }
+    });
+    return isSame;
 }
 
 module.exports = compare_collections;
