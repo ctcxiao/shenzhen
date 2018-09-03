9bf0faa08a2b748db556c052cfde2d94f473704f
tcchen <tcchen@thoughtworks.com>
2018-09-03 10:36:28 +0800

    [Cheng] implement get_integer_interval function.

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,23 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  var result = [];
+  if (number_a === number_b) {
+    result[0] = number_b;
+    return result;
+  }
+
+  if (number_b > number_a) {
+    for (var i = number_a; i <= number_b; i++) {
+      result[i-1] = i;
+    }
+  } else {
+    for (var i = number_a; i >= number_b; i--) {
+      result[number_a-i] = i;
+    }
+  }
+
+  return result;
 }
 
 module.exports = get_integer_interval;
