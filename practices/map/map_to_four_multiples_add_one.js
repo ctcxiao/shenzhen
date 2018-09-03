ec24916092e361ba80f686baee3fc2db2b26b91f
tcchen <tcchen@thoughtworks.com>
2018-09-03 11:07:56 +0800

    [Cheng] implement map to three muptiples function.

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -2,6 +2,7 @@
 var map_to_four_multiples_add_one = function(collection){
   return collection.map(function (value) {
     return value*4+1;
-  });};
+  });
+};
 
 module.exports = map_to_four_multiples_add_one;
