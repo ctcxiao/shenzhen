79919c9809a36d6e0f10ffbdea187c70f0bfae26
tcchen <tcchen@thoughtworks.com>
2018-09-03 20:29:20 +0800

    [Cheng] commit grouping same element count function.

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,7 +1,31 @@
 'use strict';
 
-function grouping_count(collection) {
+function createResult(map) {
+  var result = "{ ";
+  map.forEach((value, key) => {
+    result += key + " : " + value + ", ";
+  });
+  result = result.substring(0, result.length - 2);
+  result += " }";
+  return eval('('+result+')');
+}
 
+function countSameElement(map, value) {
+  if (map.has(value)) {
+    let count = map.get(value);
+    count++;
+    map.set(value, count);
+  } else {
+    map.set(value, 1);
+  }
+}
+
+function grouping_count(collection) {
+  var map = new Map();
+  collection.forEach((value) => {
+    countSameElement(map, value);
+  });
+  return createResult(map);
 }
 
 module.exports = grouping_count;
