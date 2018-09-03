60982e881cb7f8914f4c7646c328074d754d72cb
tcchen <tcchen@thoughtworks.com>
2018-09-03 10:49:37 +0800

    [Cheng] add choose_no_repeat_number function

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,6 +3,27 @@
 function choose_no_repeat_number(collection) {
 
   //在这里写入代码
+  var result = [];
+  var i = 0;
+  var isExist = false;
+  collection.forEach(function (value) {
+
+     result.forEach(function (value2) {
+        if (value2 === value){
+          isExist = true;
+        }
+     });
+
+     if (!isExist){
+       result[i] = value;
+       i++;
+     }
+
+     isExist = false;
+
+  });
+
+  return result;
 }
 
 module.exports = choose_no_repeat_number;
