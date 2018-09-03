6e1ee9e2fda7918019245ffa30f1764572338897
tcchen <tcchen@thoughtworks.com>
2018-09-03 10:58:28 +0800

    [Cheng] add two_collections_practice_1 function

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,7 +2,6 @@
 
 function grouping_count(collection) {
 
-  //在这里写入代码
 }
 
 module.exports = grouping_count;
--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -3,6 +3,28 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var result = [];
+  var i = 0;
+  var isExist = false;
+
+  collection_a.forEach(function (value) {
+      collection_b.forEach(function (value2) {
+          if (value === value2){
+            isExist = true;
+          }
+      });
+
+      if (isExist){
+        result[i] = value;
+        i++;
+      }
+
+      isExist = false;
+  });
+
+
+
+  return result;
 }
 
 module.exports = choose_common_elements;
