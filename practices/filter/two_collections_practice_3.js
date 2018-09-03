8a4231a0977ca61c0169f032d01938157a0ded8f
tcchen <tcchen@thoughtworks.com>
2018-09-03 11:01:34 +0800

    [Cheng] add two_collections_practice_3 function

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,28 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  var result = [];
+  var i = 0;
+  var isExist = false;
+
+  collection_a.forEach(function (value) {
+    collection_b.forEach(function (value2) {
+      if (value % value2 === 0){
+        isExist = true;
+      }
+    });
+
+    if (isExist){
+      result[i] = value;
+      i++;
+    }
+
+    isExist = false;
+  });
+
+
+
+  return result;
 }
 
 module.exports = choose_divisible_integer;
