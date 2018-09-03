1d50680593f409252ff6dadd91ff0e4d433aeae1
tcchen <tcchen@thoughtworks.com>
2018-09-04 00:07:56 +0800

    [Cheng] finish collection operator work.

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,51 @@
 'use strict';
 
+function createArray(number_b, number_a, temp) {
+  if (number_b > number_a) {
+    for (let i = number_a; i <= number_b; i++) {
+      temp.push(i);
+    }
+  } else {
+    for (let i = number_a; i >= number_b; i--) {
+      temp.push(i);
+    }
+  }
+}
+
+function generateResult(value, result) {
+  let count = Math.floor(value / 26);
+  let which_letter = value % 26;
+  result.push(mapToLetter(count, which_letter));
+}
+
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let temp = [];
+  let result = [];
+  createArray(number_b, number_a, temp);
+
+  temp.forEach(value => {
+    generateResult(value, result);
+  });
+
+  return result;
+}
+
+function mapToLetter(count, which_letter) {
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    result += "a";
+  }
+  result = handleMapWhenNumberUpperThan9(which_letter, result);
+  return result;
+}
+
+function handleMapWhenNumberUpperThan9(which_letter, result) {
+  if (which_letter <= 9) {
+    result += String.fromCharCode((which_letter + "").charCodeAt() + 48);
+  } else {
+    result += String.fromCharCode((9 + "").charCodeAt() + 48 + which_letter - 9);
+  }
+  return result;
 }
 
 module.exports = get_letter_interval;
--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+
+function get_letter_interval(number_a, number_b) {
+  let step = number_a > number_b ? -1 : 1;
+  let arr = [];
+  for (let i = number_a; i !== number_b; i += step) {
+    arr.push(i);
+  }
+  arr.push(number_b);
+  return arr.map(value => get_letter(value));
 }
 
-module.exports = get_letter_interval_2;
+function get_letter(i) {
+  i--;
+  let letter = '';
+  while (i >= 0) {
+    letter = String.fromCharCode((97 + (i % 26))) + letter;
+    i = parseInt(i / 26) - 1;
+  }
+  return letter;
+}
 
+
+module.exports = get_letter_interval;
