2b5f90644455b12f6948a17a99ace45b074ef351
tcchen <tcchen@thoughtworks.com>
2018-09-03 10:28:46 +0800

    [Cheng] implement collect_all_even function.

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,15 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var result = [];
+  var j = 0;
+  for(var i=0; i<collection.length; i++){
+    if(collection[i]%2 === 0){
+      result[j] = collection[i];
+      j++;
+    }
+  }
+  return result;
 }
 
 module.exports = collect_all_even;
